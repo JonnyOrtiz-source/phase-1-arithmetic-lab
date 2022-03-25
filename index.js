098de20ab2147f3f01695df066400ff4c3beab20
Jonny Ortiz <j0nny.0rt1z@yahoo.com>
2022-03-24 20:10:02 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-
 // Write your code here
+const num1 = 31,
+    num2 = 2,
+    multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 10) + 1;
+
+const num3 = 29,
+    num4 = 5,
+    mod = num3 % num4;
+
+const max = Math.max(5, 10, 15, 20);
